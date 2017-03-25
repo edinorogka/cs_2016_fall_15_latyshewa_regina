c733b2e4c9ea7439b5e1f04bcc1ac74249bc5bd1
edinorogka <edinorogka@users.noreply.github.com>
2017-03-25 18:17:24 +0300

    Create 10.c

---
+++ c_task/10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+unsigned int rightrot(unsigned int x,unsigned int n)
+{
+    return (x>>n);
+}
+
+int main()
+{
+
+	unsigned int x, n;
+	printf("Vvedi x: ");
+	scanf("%i",&x);
+	printf("Vvedi n:");
+    scanf("%u", &n);
+	x = rightrot(x, n);
+	printf("%u", x);
+
+	return 0;
+}
