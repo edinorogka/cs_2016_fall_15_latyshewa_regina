64ef522374ee9b7490765907bfd1b01ab01c2bfb
edinorogka <edinorogka@users.noreply.github.com>
2017-06-04 19:40:57 +0300

    Update 10.c

--- c_task/10.c
+++ c_task/10.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-
+#include <locale.h>
+#include <math.h>
 
 unsigned int rightrot(unsigned int x,unsigned int n)
 {
-    return (x>>n);
+    int i ;
+    for(i=0;i<n;i++)
+        {
+            if((x%2) == 0)
+            {
+                x=x/2;
+            }
+            else
+            {
+                x=x/2;
+                x=x+pow(2,31);
+            }
+        }
+        return x;
 }
 
 int main()
 {
-
-	unsigned int x, n;
-	printf("Vvedi x: ");
-	scanf("%i",&x);
-	printf("Vvedi n:");
-    scanf("%u", &n);
-	x = rightrot(x, n);
-	printf("%u", x);
-
-	return 0;
+    setlocale(LC_ALL,"Rus");
+    unsigned int b,h,f;
+    printf("Введите число\n");
+    scanf("%u",&b);
+    printf("Введите разряд\n");
+    scanf("%u",&h);
+    printf("Результат %u\n",rightrot(b,h));
+    return 0;
 }
