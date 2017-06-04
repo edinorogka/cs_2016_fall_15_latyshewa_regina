24594e7e8305c98e06199577a5060f5053b4fb0c
edinorogka <edinorogka@users.noreply.github.com>
2017-06-04 19:49:02 +0300

    Create 18.c

---
+++ c_task/18.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void copy(char *c,char *x)
+{   char str[100];
+    FILE *C,*X;
+    C=fopen(c,"r");
+    X=fopen(x,"w");
+    char i;
+    while((i=fgetc(C))!=EOF)
+    {
+        fputc(i,X);
+    }
+}
+int main()
+{
+    char a[100],b[100];
+    
+    gets(a);
+   
+    gets(b);
+    copy(a,b);
+    printf("done\n");
+    return 0;
+}
