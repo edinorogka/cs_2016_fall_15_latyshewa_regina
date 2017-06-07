72292ba7f246aa27fd3f0c5a647c7f33d8eb3b43
edinorogka <edinorogka@users.noreply.github.com>
2017-06-08 02:10:44 +0300

    Update 6.c

--- c_task/6.c
+++ c_task/6.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-	int n,i,m=1;
-	scanf("%d",&n);
+    int n,i,m=1;
+    scanf("%d",&n);
 	for( i=1;i<=n;i++)
-	{
-		m=m*i;
-	}
-	printf("%i",m);
+	{m=m*i;}
+     printf("%i",m);
 	return 0;
-
 }
