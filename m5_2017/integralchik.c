177230a7f8aee5b0045657511a3ca9a716e8805e
edinorogka <edinorogka@users.noreply.github.com>
2017-06-10 12:28:40 +0300

    Create integralchik.c

--- m5_2017/integralchik.c
+++ m5_2017/integralchik.c
@@ -8,27 +8,36 @@ float func(float x)
 	return x*x;
 }
 
-int main()
+void func1 (float h = 0.0, float x = 0.0, float p = 0.0, float a = 0.0, float b = 0.0, float p1 = 0.0, float p2 = 0.0)
 {
-	setlocale(LC_CTYPE, "Russian");
-	float h = 0.0, x = 0.0, p = 0.0, a = 0.0, b = 0.0, p1 = 0.0, p2 = 0.0;
-	printf("Введите нижнюю границу:");
-	scanf("%f", &a);
-	printf("Введите верхнюю границу:");
-	scanf("%f", &b);
-	printf("Введите шаг:");
-	scanf("%f", &h);
 	x = a;
 	p1 = ((-3) * func(x) + 4 * (func(x + h)) - (func(x + 2 * h))) / (2 * h);
 	p2 = (1 / (2 * h))*(-func(x) + func(x + 2 * h));
-	printf("%f\n", p1);
-	printf("%f\n", p2);
 	while (x + 2 * h <= b)
 	{
 		p = (1 / (2 * h))*(func(x) - 4 * (func(x + h)) + 3 * (func(x + 2 * h)));
 		x = x + h;
 		printf("%f\n", p);
 	}
+}
 
+int main()
+{
+	float p2 = 0.0;
+	float p1 = 0.0;
+	float h = 0.0;
+	float b = 0.0;
+	float a = 0.0;
+	setlocale(LC_CTYPE, "Russian");
+	printf("Введите нижнюю границу:");
+	scanf("%f", &a);
+	printf("Введите верхнюю границу:");
+	scanf("%f", &b);
+	printf("Введите шаг:");
+	scanf("%f", &h);
+	printf("%f\n", p1);
+	printf("%f\n", p2);
+	func1(h, a, b);
 	return 0;
 }
+
