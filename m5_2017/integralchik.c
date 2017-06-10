8114975babe29edcaf1f6f6bc90139d922006a2d
edinorogka <edinorogka@users.noreply.github.com>
2017-06-10 13:18:07 +0300

    Update integralchik.c

--- m5_2017/integralchik.c
+++ m5_2017/integralchik.c
@@ -1,15 +1,19 @@
+#include "stdafx.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <locale.h>
-
 float func(float x)
 {
-	return x*x;
+	return 4;
 }
-
-void func1 (float h = 0.0, float x = 0.0, float p = 0.0, float a = 0.0, float b = 0.0, float p1 = 0.0, float p2 = 0.0)
+float x;
+void func1(float h, float a, float b)
 {
+	float p;
+	float p1;
+	float p2;
+	float x;
 	x = a;
 	p1 = ((-3) * func(x) + 4 * (func(x + h)) - (func(x + 2 * h))) / (2 * h);
 	p2 = (1 / (2 * h))*(-func(x) + func(x + 2 * h));
@@ -23,21 +27,14 @@ void func1 (float h = 0.0, float x = 0.0, float p = 0.0, float a = 0.0, float b 
 
 int main()
 {
+
 	float p2 = 0.0;
 	float p1 = 0.0;
-	float h = 0.0;
-	float b = 0.0;
+	float h = 1.0;
+	float b = 10.0;
 	float a = 0.0;
 	setlocale(LC_CTYPE, "Russian");
-	printf("Введите нижнюю границу:");
-	scanf("%f", &a);
-	printf("Введите верхнюю границу:");
-	scanf("%f", &b);
-	printf("Введите шаг:");
-	scanf("%f", &h);
-	printf("%f\n", p1);
-	printf("%f\n", p2);
 	func1(h, a, b);
+	printf("%f\n", func(x));
 	return 0;
 }
-
